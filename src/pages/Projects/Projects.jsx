901c425ab3e76cecd0b98694ac2f4098f48bd667
Bruno Cantarelli <cantarellibruno2004@gmail.com>
2023-11-23 14:10:18 -0300

    inicio implementação carrossel

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,26 +1,39 @@
-import './Projects.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'bootstrap';
+import "./Projects.css";
+import Carousel from "react-bootstrap/Carousel";
 
-
-function Projects(){
-    return(
-        <>
+function Projects() {
+  return (
+    <>
+      <div className="container-projects">
         <h1>Meus Projetos</h1>
         <Carousel>
-      <Carousel.Item>
-        <img src="imagem1.jpg" alt="Imagem 1" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="imagem2.jpg" alt="Imagem 2" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="imagem3.jpg" alt="Imagem 3" />
-      </Carousel.Item>
-    </Carousel>
-
-        </>
-    )
+          <Carousel.Item interval={1000}>
+            <img src="teste.png" alt="" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img src="" alt="" />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="" alt="" />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
