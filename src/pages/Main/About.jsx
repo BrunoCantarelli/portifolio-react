f81ea15fa61a1b870287eb9bc246a997e1761d67
Bruno Cantarelli <cantarellibruno2004@gmail.com>
2023-11-23 20:42:06 -0300

    correções - att 23/11

--- src/pages/Main/About.jsx
+++ src/pages/Main/About.jsx
@@ -7,20 +7,22 @@ function Main() {
         <div className="title-about">
           <h1>Sobre mim</h1>
         </div>
-        <p className="main-about">
-          Bem Vindo(a) ao meu perfil! Me chamo Bruno Cantarelli, tenho 19 anos e
-          sou estudante de Sistemas de Informação pela UFSM. No momento estou
-          fazendo cursos por fora da faculdade com foco em Front-end
-          (desenvolvimento web), e meu principal objetivo é buscar cada vez mais
-          conhecimento na area de tecnologia!
-        </p>
-        <div className="knowledge">
-          <p className="main-about">
-            Já tive contato com várias tecnologias, dentre elas tem: Javascript, VueJS, ReactJs, C, C++,
-            Python, Flutter, Dart, porém, a área que eu senti maior vontade de
-            me desenvolver foi o desenvolvimento web, onde com consegui colocar
-            farias ideias que tinha em forma de sites/projetos.
+        <div className="main-about">
+          <p className="text-about">
+            Bem Vindo(a) ao meu perfil! Me chamo Bruno Cantarelli, tenho 19 anos
+            e sou estudante de Sistemas de Informação pela UFSM. No momento
+            estou fazendo cursos por fora da faculdade com foco em Front-end, e meu principal objetivo é buscar cada vez
+            mais conhecimento na area da tecnologia!
           </p>
+          <div>
+            <p className="text-about">
+              Já tive contato com várias tecnologias, dentre elas tem:
+              Javascript, VueJS, ReactJs, C, C++, Python, Flutter, Dart, porém,
+              a área que eu senti mais a vontade de me desenvolver foi o
+              desenvolvimento web, onde consegui colocar varias ideias que tinha
+              em forma de sites/projetos.
+            </p>
+          </div>
         </div>
       </div>
     </main>
